e04f7faf101216c9e5f32e0ef68c024859cc8963
narayank97 <narayankarun@gmail.com>
2020-01-30 16:15:12 -0800

    converted to heroku app

---
+++ startServer.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+var http = require('http');
+var server = http.Server(app);
+
+// app.use(express.static(path.join(__dirname,"assets")));
+// app.use(express.static(path.join(__dirname,"css")));
+// app.use(express.static(path.join(__dirname,"imgs")));
+// app.use(express.static(path.join(__dirname,"js")));
+app.use('/',express.static(__dirname+'/public'));
+
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/public/index.html');
+})
+
+app.listen(PORT,()=>console.log(`Weather APP is listening on port ${PORT}!`));
